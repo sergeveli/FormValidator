997488811b877639298afbe13128c208f24490c1
sergeveli <70241892+sergeveli@users.noreply.github.com>
2021-06-13 04:26:29 -0400

    first round of proofreads

---
+++ script.js
@@ -0,0 +1,16 @@
+const name = document.getElementById('name')
+const password = document.getElementById('password')
+const form = document.getElementById('form')
+
+form.addEventListener('submit', (e) => {
+    let messages = []
+    if (name.value === '' || name.value == null){
+        messages.push('Name is required')
+    }
+
+    if(messages.length > 0){
+        e.preventDefault()
+        errorElement.innerText = messages.join(', ')
+
+    }
+})
